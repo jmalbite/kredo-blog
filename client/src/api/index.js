f0275f50edd206eeee0375f9efb22853c4a2c470
jmalbite <albite.jm@gmail.com>
2022-03-11 23:32:03 +0800

    adding backend to my app

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -5,5 +5,5 @@ const blogsURL = 'https://jsonplaceholder.typicode.com/posts';
 
 export const fetchAllBlogs = () => axios.get(blogsURL);
 export const addBlog = (newBlog) => axios.post(blogsURL, newBlog);
-export const updateBlog = (blogID, newBlog) => axios.put(`${blogsURL}/${blogID}`, newBlog);
+export const updateBlog = (blogID, updatedBlog) => axios.put(`${blogsURL}/${blogID}`, updatedBlog);
 export const deleteBlog = (blogID) => axios.delete(`${blogsURL}/${blogID}`);
