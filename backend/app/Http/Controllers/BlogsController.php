cfa94bf84b605480da9cdd81ee5f96ae6e7e1485
jmalbite <albite.jm@gmail.com>
2022-03-12 01:58:45 +0800

    done crud api for backend using laravel

---
+++ backend/app/Http/Controllers/BlogsController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Blogs;
+use Illuminate\Http\Request;
+
+class BlogsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //get all blogs
+        return Blogs::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //store new blog
+        $request->validate([
+            'title' => 'required',
+            'content' => 'required'
+        ]);
+
+        return Blogs::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //search blog
+        return Blogs::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //update blog
+
+        //get the blog id and the data in the database
+        $blog = Blogs::find($id);
+        //once the id is found update the blog
+        $blog->update($request->all());
+
+        return $blog;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //delete blog
+        return Blogs::destroy($id);
+    }
+
+
+
+    /**
+    * Search multiple resource from the storage
+    *
+    * @param  string  $title
+    * @return \Illuminate\Http\Response
+    */
+    public function search($title)
+    {
+        //multiple research
+        return Blogs::where('title', 'like', '%'.$title.'%')->get();
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BlogsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+// Route::get('/blogs', [BlogsController::class, 'index']);
+// Route::post('/blogs', [BlogsController::class, 'store']);
+
+
+Route::resource('blogs', BlogsController::class);
+Route::get('/blogs/search/{title}', [BlogsController::class, 'search']);
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
